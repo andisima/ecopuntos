be878015365e10cd367620fd937e9a31dc7fe70f
severo iglesias <siglesias@ucol.mx>
2019-11-02 19:09:41 -0600

    agrege vista de login

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var x3=0;
-  res.render('puntos', {puntos:x3, nombre:"sevix"});
+  var x3=22;
+  var x4=10;
+  var suma=x3+x4;
+  res.render('puntos', {nivel:suma,puntos:150, nombre:"Severo Iglesias"});
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', {});
 });
 
 module.exports = router;
